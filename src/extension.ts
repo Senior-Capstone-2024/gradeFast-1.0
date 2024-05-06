c4f12c55e4b5051fcdc5542ff57c94edfda2e9e2
willeand <eandwse@gmail.com>
2024-05-05 22:31:53 -0700

    Added line error to single datastructure, highlighting partially broken

--- src/extension.ts
+++ src/extension.ts
@@ -8,11 +8,10 @@ import { findCapitalizedMethodName } from './findCapitalizedMethodName';
 import { singleStatementPerLineChecker } from './singleStatementPerLineChecker';
 import { findAllErrors } from './findAllErrors';
 import { convertMapToJson } from './convertMapToJson';
-import { findDuplicateCode } from './findDuplicateCode';
-
 
 const myMap: Map<number, string[]> = new Map();
-const filePath = "/Users/willscomputer/Desktop/abc/gradeFast-1.0/output/data.json";
+const filePath = 'C:\\Users\\Will\\Desktop\\cw\\gradeFast-1.0\\output\\data.json';
+
 
 export function activate(context: vscode.ExtensionContext) {
     const disposable = vscode.commands.registerCommand('extension.findCapitalizedPrimitiveTypes', () => {
@@ -20,11 +19,14 @@ export function activate(context: vscode.ExtensionContext) {
         convertMapToJson(myMap, filePath);
     });
 
+    const disposable2 = vscode.commands.registerCommand('extension.findCapitalizedMethodName', () => {
+        findCapitalizedMethodName(myMap);
+    } );
+
+
     const disposable1 = vscode.commands.registerCommand('extension.findLowercaseClassOrInterface', findLowercaseClassOrInterface);
-    const disposable2 = vscode.commands.registerCommand('extension.findCapitalizedMethodName', findCapitalizedMethodName);
     const disposable3 = vscode.commands.registerCommand('extension.singleStatementPerLineChecker', singleStatementPerLineChecker);
     const disposable0 = vscode.commands.registerCommand('extension.findAllErrors', findAllErrors);
-    const disposable4 = vscode.commands.registerCommand('extension.findDuplicateCode', findDuplicateCode);
 
     const commentController = vscode.comments.createCommentController('comment-sample', 'Comment API Sample');
     context.subscriptions.push(commentController);
--- src/findCapitalizedMethodName.ts
+++ src/findCapitalizedMethodName.ts
@@ -1,6 +1,6 @@
 import * as vscode from 'vscode';
 
-export function findCapitalizedMethodName() {
+export function findCapitalizedMethodName(myMap: Map<number, string[]>): Map<number, string[]> {
 	// activeTextEditor allows access to text inside the opened document.
     const editor = vscode.window.activeTextEditor;
         if (editor) {
@@ -11,6 +11,7 @@ export function findCapitalizedMethodName() {
             const improperMethodNames = [];
             while ((match = javaMethodRegex.exec(text))) {
                 const methodName = match[1];
+                const lineNumber = document.positionAt(match.index).line + 1; // Get the line number
                 if (!isValidMethodName(methodName)) {
                     const methodStartPosition = document.positionAt(match.index);
                     const methodEndPosition = document.positionAt(match.index + methodName.length);
@@ -18,13 +19,16 @@ export function findCapitalizedMethodName() {
                     improperMethodNames.push(range);
                     vscode.window.showInformationMessage('Java Method Name Convention Mistake!! Highlighted in RED');
                 }
-            }
-            if (improperMethodNames.length > 0) {
-                editor.setDecorations(improperMethodNameDecorationType, improperMethodNames);
-            } else {
-                vscode.window.showInformationMessage('All Java method names follow proper conventions!');
+                if (!myMap.has(lineNumber)) {
+                    myMap.set(lineNumber, []);
+                }
+                const errorMessage = "Improper Method Name";
+                const currentErrors = myMap.get(lineNumber) || [];
+                currentErrors.push(errorMessage);
+                myMap.set(lineNumber, currentErrors);
             }
         }
+        return myMap;
     }
 
     const improperMethodNameDecorationType = vscode.window.createTextEditorDecorationType({
